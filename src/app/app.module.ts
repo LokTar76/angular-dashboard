b1eb4cf62dfcd5f88f175fc622d61fa2ac9ecc91
LokTar76 <omg23333@gmail.com>
2022-06-17 01:55:22 +1000

    Created feedback charts dashboard, created pie chart with mock data.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,52 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSliderModule } from '@angular/material/slider';
 
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+
+import {MatSidenavModule} from "@angular/material/sidenav";
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatListModule } from '@angular/material/list';
+import { FeedbackChartComponent } from './feedback-chart/feedback-chart.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { FeedbackPieComponent } from './feedback-chart/charts/feedback-pie/feedback-pie.component';
+import {NgChartsModule} from "ng2-charts";
+import { FeedbackTableComponent } from './feedback-table/feedback-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FeedbackChartComponent,
+    FeedbackPieComponent,
+    FeedbackTableComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    LayoutModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    NgChartsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
